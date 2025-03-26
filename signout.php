66e4bb143a66069ae48f89a8fb8edf162650c9a4
satkrit <satkrit15@gmail.com>
2025-03-26 11:24:34 +0545

    signout

--- signout.php
+++ signout.php
@@ -1,6 +1,11 @@
 <?php
- session_start();
- session_unset();
- session_destroy();
- header("Location:home.html");
+session_start();
+session_unset();
+session_destroy();
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+header("Location: home.html");
+exit();
 ?>
